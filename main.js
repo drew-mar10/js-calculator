55084fd39c15fa08381f0f767c6f3189c7c01911
Kalen Martin <kalen.martin1@gmail.com>
2022-02-05 15:07:27 -0500

    empty node list

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+let buttons = document.querySelectorAll("button");
+console.log(buttons)
+
+//buttons.forEach(function (inner) {
+//    console.log("inner")
+//inner.addEventListener('click', function (event) {
+//    console.log(event.target.innerText)
+//    console.log('clicked')})
+//})
