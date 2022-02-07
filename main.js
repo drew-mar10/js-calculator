fdd0c73694a35a5a58a17f5fd2081ef515fa9f9e
Kalen Martin <kalen.martin1@gmail.com>
2022-02-07 00:07:18 -0500

    disaster pt deux

--- main.js
+++ main.js
@@ -3,27 +3,34 @@ console.log('equals')
 
 //const keys = ['grid-cell-1', 'grid-cell-2', 'grid-cell-3', 'grid-cell-4', 'grid-cell-5', 'grid-cell-6', 'grid-cell-7', 'grid-cell-8', 'grid-cell-9', 'grid-cell-10', 'grid-cell-11', 'grid-cell-12', 'grid-cell-13', 'grid-cell-14', 'grid-cell-15', 'grid-cell-16', 'grid-cell-17', 'grid-cell-18', 'grid-cell-19', 'grid-cell-20']
 
-let eval = document.getElementById('eval');
+let display = document.getElementById('display');
+let clear = document.getElementById('clear');
+let equals = document.getElementById('equals');
 
-const numButtons = document.querySelectorAll(".button");
-console.log("buttons array", numButtons);
+const calcButtons = document.querySelectorAll(".button");
+console.log("buttons array", calcButtons);
 
-//selects the DOM to isolate the display window and makes it into a variable called display
-const display = document.getElementById("display");
-console.log("display");
+const dataButtons = document.querySelectorAll(".data button");
+console.log("data buttons array", dataButtons);
 
-for (let button of numButtons) {
-    button.addEventListener('click', (button) => {
-        display.innerText += button.innterText
-        }); //this allows us to log in the display over and over
+const funcButtons = document.querySelectorAll(".func button");
+console.log("func buttons array", funcButtons);
+
+const equalsButton = document.querySelector('[equals]');
+console.log("equals", equals);
+
+const clearButton = document.querySelector('[clear]');
+console.log("clear", clear);
+
+
+
+let clicked = document.querySelector('.button')
+clicked.addEventListener('click', function () {
+    console.log(clicked)
+})
+
+for (let button of calcButtons) {
+    button.addEventListener('click', (clicked) => {
+        console.log(clicked);
+        });
     };
-
-    clear.addEventListener('click', () => {
-        display.innterText = " "
-    })
-
-    eval.addEventListener('click', () => {
-        console.log("button clicked")
-        display.innterText = eval(display.innterText)
-        console.log("button clicked")
-    });
